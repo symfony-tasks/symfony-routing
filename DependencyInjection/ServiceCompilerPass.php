cc0bf0c83f317e67223b3adf71bb3e25d5645451
nighten <nighten@banki.ru>
2018-01-18 17:27:52 +0300

    tasks 1-4

--- DependencyInjection/ServiceCompilerPass.php
+++ DependencyInjection/ServiceCompilerPass.php
@@ -3,13 +3,16 @@
 namespace BankiruSchool\Routing\DependencyInjection;
 
 use BankiruSchool\Routing\Common\AbstractCompilerPass;
+use BankiruSchool\Routing\Tasks\KernelEvents;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 final class ServiceCompilerPass extends AbstractCompilerPass
 {
     protected function getListeners(): array
     {
-        return [];
+        return [
+            [KernelEvents::class, 'request']
+        ];
     }
 
     protected function doAdditionalConfiguration(ContainerBuilder $builder)
